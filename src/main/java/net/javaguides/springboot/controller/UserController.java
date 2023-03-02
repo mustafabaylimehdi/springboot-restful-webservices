e29d3f9e63717073485753c99266ba9015cd78d5
Mehdi M. Mustafabeyli <MustafabeyliMM@kapitalho.az>
2023-03-02 15:40:21 +0400

    Initial commit

---
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -0,0 +1,64 @@
+package net.javaguides.springboot.controller;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.exception.ErrorDetails;
+import net.javaguides.springboot.exception.ResourceNotFoundException;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+
+    private UserService userService;
+
+    // build create User REST API
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user){
+        UserDto savedUser = userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    //build get user by id REST API
+    // http://localhost:8080/api/users/1
+    @GetMapping("{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId){
+        UserDto userDto = userService.getUserById(userId);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+    //build get all users REST API
+    // http://localhost:8080/api/users
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        List<UserDto> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    //build update user REST API
+    // http://localhost:8080/api/users/1
+    @PutMapping("{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId, @RequestBody @Valid UserDto user){
+        user.setId(userId);
+        UserDto updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    //build delete user REST API
+    // http:localhost:8080/api/users/1
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("User successfully deleted", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/net/javaguides/springboot/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package net.javaguides.springboot.mapper;
+
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+
+public class UserMapper {
+    public static UserDto mapperToUserDto(User user){
+        //Convert User JPA Entity to UserDto
+        UserDto userDto = new UserDto(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail()
+        );
+        return userDto;
+    }
+
+    //Convert UserDto to User JPA Entity
+    public static User mapperToUser(UserDto userDto){
+        User user = new User(
+                userDto.getId(),
+                userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getEmail()
+        );
+        return user;
+    }
+}
